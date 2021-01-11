8c251513667703f112e6aaa385380b7e0371fd5e
Aramide Oluwatosin <aoluwatosin10@gmail.com>
2021-01-11 01:27:46 +0000

    ENAUTO

--- Netconf_IOS_XE/ENAUTOrestconfDELETE.py
+++ Netconf_IOS_XE/ENAUTOrestconfDELETE.py
@@ -16,6 +16,20 @@ headers = {
     "Content-Type": "application/yang-data+json"
 }
 
+url =f"https://{router['host']}:{router['port']}/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces/interface=Loopback100"
+
+response = requests.get(
+    url=url,
+    headers=headers,
+    auth=(
+        router['user'],
+        router['password']
+    ),
+    verify=False,
+).json()
+
+print(json.dumps(response, indent=2))
+
 # url = f"https://{router['host']}:{router['port']}/restconf/data/ietf-interfaces:interfaces/interface=Loopback100"
 url = f"https://{router['host']}:{router['port']}/restconf/data/Cisco-IOS-XE-native:native/interface/Loopback=100"
 
